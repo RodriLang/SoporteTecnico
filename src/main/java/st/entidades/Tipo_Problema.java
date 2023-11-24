055295dc75426c240ae563366d39f6942dd872a3
matias-pacheco <matias-pacheco@outlook.es>
2023-11-24 20:52:33 -0300

    Clase Tipo_Problema

---
+++ src/main/java/st/entidades/Tipo_Problema.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package st.entidades;
+
+/**
+ *
+ * @author Matías Pacheco
+ */
+public class Tipo_Problema {
+    private int id;
+    private String tipo; //posible ENUM
+}
