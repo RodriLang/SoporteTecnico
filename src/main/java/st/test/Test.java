dad70112ebc2a74698fc550246425832ec1b8a81
matias-pacheco <matias-pacheco@outlook.es>
2023-11-25 16:22:31 -0300

    Configuración de persistence y gestion del POM
    - Configure el archivo persistence.xml para realizar una conexión con la DB utilizando hibernate.
    - Añadí la dependencia de mariadb en el POM.

--- src/main/java/st/test/Test.java
+++ src/main/java/st/test/Test.java
@@ -1,11 +1,21 @@
 package st.test;
 
-import st.gui.MesaDeEntrada;
-import st.entidades.Operador;
+import st.entidades.Tecnico;
+import st.repositorios.TecnicoRImplement;
 public class Test {
 
  
     public static void main(String[] args) {
+        TecnicoRImplement tecnicoRI = new TecnicoRImplement();
         
+        Tecnico t1 = Tecnico.builder()
+                .matricula(76767)
+                .dni(30444555)
+                .apellido("Gimenez")
+                .nombre("Julia")
+                .estado(true)
+                .build();
+        
+        tecnicoRI.add(t1);
     }
 }
